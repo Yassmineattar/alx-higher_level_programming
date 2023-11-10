154affce475303ecf1e383e65ca73148c1e940a4
Yassmineattar <yassmineattar13@gmail.com>
2023-11-10 01:12:35 +0100

    tasks

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary or a_dictionary is None:
+        return None
+    best = max(a_dictionary, key=a_dictionary.get)
+    return (best)
