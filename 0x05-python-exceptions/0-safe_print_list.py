d9010dafeedaf3f711c8973bc7bd78a94630394a
Yassmineattar <yassmineattar13@gmail.com>
2023-11-21 02:01:38 +0100

    a

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        c = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            c += 1
+        print()
+    except IndexError:
+        print()
+    finally:
+        return (c)
