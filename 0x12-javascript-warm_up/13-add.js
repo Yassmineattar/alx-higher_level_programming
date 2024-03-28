308078d39a694f914b28fe59099bf77efb7aa749
Yassmineattar <yassmineattar13@gmail.com>
2024-03-28 16:34:41 +0000

    semistandard validation

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 function add (a, b) {
-    return parseInt(a) + parseInt(b);
-  }
-  
-  module.exports = {
-    add: add
-  };
+  return parseInt(a) + parseInt(b);
+}
+
+module.exports = {
+  add: add
+};
