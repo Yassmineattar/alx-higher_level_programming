e33261aab8e229903f5958b2d83ecb6e057fb336
Yassmineattar <yassmineattar13@gmail.com>
2024-03-28 02:45:33 +0000

    tests

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#!This Bash script takes in a URL, sends a request to that URL
+curl -sI "$1" | awk '/Content-Length/ {print $2}'
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#This bash script sends a DELETE request to the URL
+curl -s "$1" -X DELETE
