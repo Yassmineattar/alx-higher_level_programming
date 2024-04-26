f6e5b68b37e53504ca0c98c52c802fec46ff79d5
Yassmineattar <yassmineattar13@gmail.com>
2024-04-26 22:30:58 +0100

    update task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,22 @@
 #!/usr/bin/node
+
 const request = require('request');
-const urlId = 'https://swapi-api.alx-tools.com/api/people/18/';
+const characterId = '18';
 const url = process.argv[2];
 let count = 0;
 
-request.get(url, (error, response, body) => { // perform an HTTP GET request
+request.get(url, (error, response, body) => {
   if (error) {
-    return console.log(error);// Print the error if one occurred
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    data.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(characterId)) {
+          count += 1;
+        }
+      });
+    });
+    console.log(count);
   }
-  const bod = JSON.parse(body);
-  bod.results.forEach(film => {
-    if (film.characters.includes(urlId)) {
-      count++;
-    }
-  });
-  console.log(count); // Print the title of the movie if a response was received
 });
